212f8a70912715b37650e472b184d44f75c73c38
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2018-02-14 19:07:12 +0100

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Andrii Kuzminskyi on 14/02/2018.
+//  Copyright © 2018 A.Kuzminskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
