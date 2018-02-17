669e81828157034fef159fda25bc2b44421571aa
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2018-02-17 23:32:03 +0100

    Added README.md; todo list moved to README.md

---
+++ MovieDB/Services/Paginator/Extensions/RESTPaginatorLoader+RESTPaginatorLoaderable.swift
@@ -0,0 +1,9 @@
+//
+//  RESTPaginatorLoader+RESTPaginatorLoaderable.swift
+//  MovieDB
+//
+//  Created by Andrii Kuzminskyi on 17/02/2018.
+//  Copyright © 2018 A.Kuzminskyi. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,4 @@
+TODO:
+
+- rename ResultItemType, ItemType, Item to Element;
+- add func cancelAllOperations() to RESTPaginatorLoaderable;
