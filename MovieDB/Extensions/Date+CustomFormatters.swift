65b2a614994302d9b235270ba50a6a00c9f1f6ab
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2018-02-18 19:13:29 +0100

    Added custom date formatter "yyyy-MM-dd"

---
+++ MovieDB/Extensions/Date+CustomFormatters.swift
@@ -0,0 +1,20 @@
+//
+//  Date+CustomFormatters.swift
+//  MovieDB
+//
+//  Created by Andrii Kuzminskyi on 18/02/2018.
+//  Copyright © 2018 A.Kuzminskyi. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let yyyyMMdd: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+}
