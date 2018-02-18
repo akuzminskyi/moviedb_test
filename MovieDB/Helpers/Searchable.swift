f1f524947cd059e5ab88d18a3655d2a569413803
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2018-02-18 19:15:52 +0100

    Added Searchable protocol

---
+++ MovieDB/Helpers/Searchable.swift
@@ -0,0 +1,13 @@
+//
+//  Searchable.swift
+//  MovieDB
+//
+//  Created by Andrii Kuzminskyi on 18/02/2018.
+//  Copyright © 2018 A.Kuzminskyi. All rights reserved.
+//
+
+import Foundation
+
+protocol Searchable {
+    func search(term: String)
+}
---
+++ MovieDB/Scenes/Search/Extensions/SearchViewModel+Searchable.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewModel+Searchable.swift
+//  MovieDB
+//
+//  Created by Andrii Kuzminskyi on 18/02/2018.
+//  Copyright © 2018 A.Kuzminskyi. All rights reserved.
+//
+
+import Foundation
+
+extension SearchViewModel: Searchable {
+    func search(term: String) {
+
+    }
+}
